4a8ebcc705dc84739d1240dea5b240ff1ac4402d
Gopikumar <gopi7dhoni@gmail.com>
2022-07-21 12:52:26 +0530

    all files and folders are uploaded

---
+++ hardhat.config.js
@@ -0,0 +1,15 @@
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  solidity: {
+    version: "0.8.0",
+  },
+  paths: {
+    artifacts: "./client/src/artifacts",
+  },
+  networks:{
+    hardhat: {
+      chainId: 1337
+    }
+  }
+};
